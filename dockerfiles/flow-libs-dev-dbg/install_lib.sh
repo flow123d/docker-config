2f5dcc1c59cea96aff14a8f08fda2e96f26d4843
Jan_Hybs <jan.hybs@tul.cz>
2016-08-30 13:44:06 +0200

    Edit lib intall script

--- dockerfiles/flow-libs-dev-dbg/install_lib.sh
+++ dockerfiles/flow-libs-dev-dbg/install_lib.sh
@@ -2,15 +2,15 @@
 # script will install library given by library name ($1), version ($2) and 
 # build type ($3)
 #
-
+SERVER=http://ciflow.nti.tul.cz/static/packages
 
 # check acceptable BUILD_TYPE argument
 if [ "${3^^}" == "DEBUG" ]; then
     FILE=${1^^}_${2}.deb
-    URL=http://ciflow.nti.tul.cz/packages/${1^^}_${2}_Debug/${FILE}
+    URL=${SERVER}/${1^^}_${2}_Debug/${FILE}
 elif [[ "${3^^}" == "RELEASE" ]]; then
     FILE=${1^^}_${2}.deb
-    URL=http://ciflow.nti.tul.cz/packages/${1^^}_${2}_Release/${FILE}
+    URL=${SERVER}/${1^^}_${2}_Release/${FILE}
 else
     echo "Invalid BUILD_TYPE argument '${3}'"
     exit 1
@@ -22,7 +22,7 @@ fi
 # after installation we remove deb package
 cd /tmp
 echo "Downloading ${FILE} from ${URL}"
-wget ${URL}
+wget --progress=dot:mega ${URL}
 dpkg -i ${FILE}
 ls -la
 rm -f ${FILE}
