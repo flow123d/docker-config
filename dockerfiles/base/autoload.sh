42d54d08f430ba4657719f24a1672a2ab9fd2a4b
Jan_Hybs <jan.hybs@tul.cz>
2016-09-05 15:11:47 +0200

    Add more tools to base image

---
+++ dockerfiles/base/autoload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# bash which will be invoked with source (.) on login
+# author: Jan Hybs
+
+# source bash_completion
+if [ -f /etc/bash_completion ]; then
+ . /etc/bash_completion
+fi
+
