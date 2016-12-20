25cd6f528c3e3575f814f4563e2b766ceb06e8db
Jan Hybs <jan.hybs@tul.cz>
2016-12-20 16:28:16 +0100

    Add module load

--- cmakefiles/build-flow.sh
+++ cmakefiles/build-flow.sh
@@ -15,6 +15,32 @@ FLOW_LOC=/storage/praha1/home/jan-hybs/projects/Flow123dDocker/flow123d
 BRANCH=JHy_runtest_update
 _SEP_="--------------------------------------------------------------------------"
 
+
+# ------------------------------------------------------------------------------
+# ----------------------------------------------------------------- MODULES ----
+# ------------------------------------------------------------------------------
+echo "Loading modules"
+echo $_SEP_
+module load cmake-2.8.12
+module load gcc-4.9.2
+module load boost-1.56-gcc
+module load perl-5.20.1-gcc
+module load openmpi
+
+module load python27-modules-gcc
+module load python-2.7.6-gcc      # version 2.7.10-gcc has issue when importing hashlib 
+                                  # > python
+                                  # import hashlib
+                                  # <ERROR>
+
+module unload gcc-4.8.1
+module unload openmpi-1.8.2-gcc
+
+echo "Modules loaded"
+module list
+echo $_SEP_
+
+
 # ------------------------------------------------------------------------------
 # ---------------------------------------------------------- BUILD FLOW123d ----
 # ------------------------------------------------------------------------------
