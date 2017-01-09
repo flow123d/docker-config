2870eb46b644bc63e3de2b7b4bad7b715b6812b7
Jan_Hybs <jan.hybs@tul.cz>
2017-01-09 14:57:08 +0100

    Fix flow location

--- package/project/src/linux/flow123d.sh
+++ package/project/src/linux/flow123d.sh
@@ -4,4 +4,4 @@
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # run docker shell within current dirrectory
-docker run -ti --rm -v "/$HOME:/$HOME" -w "/$(pwd)" -u $(id -u):$(id -g) "@IMAGE_TAG@:user" /opt/flow123d/flow123d/bin/flow123d "$@"
+docker run -ti --rm -v "/$HOME:/$HOME" -w "/$(pwd)" -u $(id -u):$(id -g) "@IMAGE_TAG@:user" /opt/flow123d/bin/flow123d "$@"
