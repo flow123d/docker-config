f932c64d0f7b41ce5b649639bb334b437b51dff5
Jan_Hybs <jan.hybs@tul.cz>
2017-01-23 14:49:52 +0100

    Fix flow123d.sh, add more user friendly exit of fterm

--- package/project/src/linux/flow123d.sh
+++ package/project/src/linux/flow123d.sh
@@ -4,4 +4,4 @@
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # run docker shell within current dirrectory
-docker run -ti --rm -v "/$HOME:/$HOME" -w "/$(pwd)" -u $(id -u):$(id -g) "@IMAGE_TAG@:user" /opt/flow123d/bin/flow123d "$@"
+$CWD/fterm.sh //opt/flow123d/bin/flow123d "$@"
--- package/project/src/linux/fterm.sh
+++ package/project/src/linux/fterm.sh
@@ -18,3 +18,14 @@ else
     echo "Executing $@"
     docker run -ti --rm -v "/$HOME:/$HOME" $EXTRA_MOUNT -w "/$(pwd)" -u $(id -u):$(id -g) "@IMAGE_TAG@:user" "$@"
 fi
+
+
+# check exit code
+# if we detect error, we exit console
+# only after user preses enter (user thus can see error messages or output)
+RC=$?
+if [ $RC -ne 0 ]; then
+    echo -e "\n\n\n"
+    echo "Command failed (exit code: $RC)"
+    read -p "Press enter to exit"
+fi
