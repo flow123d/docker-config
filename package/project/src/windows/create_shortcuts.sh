2f6d52a46f2d5502be11dd24fc1e071910b57b18
Jan Brezina <jan.brezina@tul.cz>
2017-11-08 18:31:59 +0100

    Fix fterm and flow123d for windows.

--- package/project/src/windows/create_shortcuts.sh
+++ package/project/src/windows/create_shortcuts.sh
@@ -13,7 +13,7 @@ cat > bin/fterm.bat << EOL
 @echo off
 docker-machine start default
 @FOR /f "tokens=*" %%i IN ('docker-machine env') DO @%%i
-start "Fterm" "$1" "$2\bin\fterm.sh" %*
+start "Fterm" /wait "$1" "$2\bin\fterm.sh" %*
 EOL
 
 
@@ -23,7 +23,10 @@ cat > bin/flow123d.bat << EOL
 @echo off
 docker-machine start default
 @FOR /f "tokens=*" %%i IN ('docker-machine env') DO @%%i
-start "Flow123d" "$1" "$2\bin\flow123d.sh" %*
+echo "Starting Flow123d."
+echo "flow123d" %* 
+start "Flow123d" /wait "$1" "$2\bin\flow123d.sh" %*
+echo "Flow123d done."
 EOL
 
 
