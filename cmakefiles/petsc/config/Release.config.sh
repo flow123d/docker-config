b1bd7660b1d4844ea91ed872797899fc9ef956a6
Jan Hybs <jan.hybs@tul.cz>
2017-11-28 11:56:24 +0000

    Edit libs creation

--- cmakefiles/petsc/config/Release.config.sh
+++ cmakefiles/petsc/config/Release.config.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-mkdir -p @PETSC_DIR@
-cp -r @INSTALL_DIR@/src/* @PETSC_DIR@
-cd @PETSC_DIR@
-
-# by default petsc will found default system libs such as blas lapack or openmpi
+unset MAKEFLAGS
 ./configure \
         PETSC_ARCH=@PETSC_ARCH@ \
+        --prefix=@PETSC_DIR@ \
         --download-metis=yes --download-parmetis=yes \
         --download-blacs=yes --download-scalapack=yes --download-mumps=yes --download-hypre=yes \
         --with-debugging=0 --with-shared-libraries=0 \
         --with-make-np @MAKE_NUMCPUS@ --CFLAGS="-O3" --CXXFLAGS="-O3 -Wall -Wno-unused-local-typedefs -std=c++11"
-
-# other options
-# --with-mpi-dir=@MPI_DIR@ \
-# --with-cc=@CMAKE_C_COMPILER@ --with-cxx=@CMAKE_CXX_COMPILER@ --with-fc=@CMAKE_Fortran_COMPILER@
-# --download-fblaslapack --download-mpich
