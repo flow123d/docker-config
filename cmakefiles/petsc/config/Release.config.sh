316cfa3cd33fe4da47eb6e7b2d0974c30f6c54a6
Jan Hybš <x3mSpeedy@gmail.com>
2018-02-06 10:07:55 +0000

    Add --with-mpi-dir  option

--- cmakefiles/petsc/config/Release.config.sh
+++ cmakefiles/petsc/config/Release.config.sh
@@ -4,6 +4,9 @@ unset MAKEFLAGS
         PETSC_ARCH=@PETSC_ARCH@ \
         --prefix=@PETSC_DIR@ \
         --download-metis=yes --download-parmetis=yes \
-        --download-blacs=yes --download-scalapack=yes --download-mumps=yes --download-hypre=yes \
+        --download-blacs=yes --download-scalapack=yes \
+        --download-mumps=yes --download-hypre=yes \
         --with-debugging=0 --with-shared-libraries=0 \
-        --with-make-np @MAKE_NUMCPUS@ --CFLAGS="-O3" --CXXFLAGS="-O3 -Wall -Wno-unused-local-typedefs -std=c++11"
+        --with-make-np @MAKE_NUMCPUS@ \
+        --CFLAGS="-O3" \
+        --CXXFLAGS="-O3 -Wall -Wno-unused-local-typedefs -std=c++11" @CONFIGURE_OPTS@
