cc1f3a1287cc369388af7c5c9c67ab4e437746fc
martinspetlik <martin.spetlik>
2021-09-08 17:27:03 +0200

    common package

--- package/intel/project/src/linux/flow123d.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script will run flow123d with given parameters
-
-CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# run docker shell within current dirrectory
-"$CWD/fterm.sh" //opt/flow123d/bin/flow123d "$@"
--- package/intel/project/src/windows/win/download-dfw.ps1
+++
@@ -1,3 +0,0 @@
-$url = "http://download.docker.com/win/stable/Docker for Windows Installer.exe"
-$output = "dfw.exe"
-Start-BitsTransfer -Source $url -Destination $output
