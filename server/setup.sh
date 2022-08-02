fbd8c798a32f417fd3b3a2f69972d0e86c3bc2c9
Jan Brezina <jan.brezina@tul.cz>
2022-08-02 10:16:41 +0200

    Fix, docker and dockerfile compose

--- server/setup.sh
+++ server/setup.sh
@@ -11,7 +11,18 @@
 
 script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 
+# function instal_docker_compose {
+#     DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+#     mkdir -p $DOCKER_CONFIG/cli-plugins
+#     curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+# }
+
 function setup_server {
+#     if ! docker compose --help >/dev/null;
+#     then install_docker_compose
+#     fi
+      
+
     docker compose rm --stop --force --volumes 
     docker volume rm jenkins-ciflow
     
